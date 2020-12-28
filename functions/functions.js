668026b0bdf296e0ffe8589fe6e4210e9c8e12c5
Tommaso Siligardi <tomesse@outlook.it>
2020-12-28 15:49:47 +0100

    provato ad esportare una singola funzione, provare a farne due

---
+++ functions/functions.js
@@ -0,0 +1,19 @@
+/*export function maggiorenne(eta){
+    if (eta>17){
+        return ("sei maggiorenne ")
+    }
+    else{return ("sei minorenne")}
+};
+export function saluti(nome){
+    let saluto="ciao" + nome
+    if (nome ==="tommaso"){
+        saluto+="sei il mio padrone"
+    }
+    return saluto
+}*/
+module.exports= function maggiorenne(eta){
+    if (eta>17){
+        return ("sei maggiorenne ")
+    }
+    else{return ("sei minorenne")}
+};
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express")
 app = new express()
 const port=8080
-
+/*const functions= require("./functions/functions.js") 
+let { maggiorenne , saluti } =functions*/
+var maggiorenne =require ("./functions/functions.js")
 app.use(express.static('public'))
 
 app.get("/",(req,res)=>{
     res.send("/public/index.html")
 })
+app.get("/eta",(req,res)=>{
+    const anni=req.query.anni
+    const stringa=maggiorenne(anni)
+    res.send(`<h1>Sei maggiorenne o minorenne?</h1>
+    <b>hai ${anni} anni quindi ${stringa}`)
+})
+
 app.listen(port,()=>console.log(`server avviato sulla porta ${port}`))
